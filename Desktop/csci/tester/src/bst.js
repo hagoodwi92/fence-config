ec3d8ce38f787c233f16ab191c3bcd0039b87f3a
hagoodwin <hagoodwi@gmail.com>
2021-04-07 11:25:04 -0700

    add bst progress

---
+++ Desktop/csci/tester/src/bst.js
@@ -0,0 +1,15 @@
+export default class BST{
+  constructor(){
+    this.root = null;
+  }
+
+  insert(node){
+    if (this.root === null) {
+      this.root = node;
+    } else if (this.root.data > node.data) {
+      this.root.left = node;
+    } else if (this.root.data < node.data) {
+      this.root.right = node;
+    }
+}
+}
