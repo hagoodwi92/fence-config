f3077fb5e74a0885d16dc3e34dbf94ee89af22ba
hagoodwin <hagoodwi@gmail.com>
2021-04-14 11:16:04 -0700

    creata basic node

--- Desktop/csci/tester/_tests_/bst-node.test.js
+++ Desktop/csci/tester/_tests_/bst-node.test.js
@@ -4,8 +4,8 @@ describe('bstNode', () => {
 
   test('should correctly create a node', () => {
     const node = new BSTNode(36);
-    expect(node.data).toEqual(36);
     expect(node.left).toEqual(null);
     expect(node.right).toEqual(null);
+    expect(node.data).toEqual(36);
   });
 });
