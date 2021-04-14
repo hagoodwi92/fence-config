dae2f6cd481c2cdca8b5753ff4dc9e0d4485c007
hagoodwin <hagoodwi@gmail.com>
2021-04-14 13:09:59 -0700

    build and traverse bst

--- Desktop/csci/tester/_tests_/bst.test.js
+++ Desktop/csci/tester/_tests_/bst.test.js
@@ -12,9 +12,67 @@ describe('binarySearchTree', () => {
   test('it should create a new root node', () => {
     let bst = new BST();
     let node = new BSTNode(36);
-    bst.insertNode(node);
+    bst.insert(node);
     expect(bst.root).toEqual(node);
   });
 
+  test('it should add a child node to the left of the root node', () => {
+    let bst = new BST();
+    let rootNode = new BSTNode(36);
+    bst.insert(rootNode);
+    let newNode = new BSTNode(22);
+    bst.insert(newNode);
+    expect(rootNode.left.data).toEqual(22);
+  });
 
+  test('it should add a child node to the right of the root node', () => {
+    let bst = new BST();
+    let rootNode = new BSTNode(36);
+    bst.insert(rootNode);
+    let newNode = new BSTNode(48);
+    bst.insert(newNode);
+    expect(rootNode.right.data).toEqual(48);
+  });
+
+  test('it should add a child node to the left of the root node', () => {
+    let bst = new BST();
+    let rootNode = new BSTNode(36);
+    bst.insert(rootNode);
+    let node2 = new BSTNode(22);
+    bst.insert(node2);
+    let node3 = new BSTNode(11);
+    bst.insert(node3);
+    expect(rootNode.left.left.data).toEqual(11);
+  });
+
+  test('it should add a child node to the right of the root node', () => {
+    let bst = new BST();
+    let rootNode = new BSTNode(36);
+    bst.insert(rootNode);
+    let node2 = new BSTNode(48);
+    bst.insert(node2);
+    let node3 = new BSTNode(55);
+    bst.insert(node3);
+    expect(rootNode.right.right.data).toEqual(55);
+  });
+
+  test('it should add a child to left or right of a node', () => {
+    let bst = new BST();
+    let rootNode = new BSTNode(36);
+    bst.insert(rootNode);
+    let node2 = new BSTNode(22);
+    bst.insert(node2);
+    let node3 = new BSTNode(33);
+    bst.insert(node3);
+    expect(rootNode.left.right.data).toEqual(33);
+  });
+  test('it should not add duplicate nodes', () => {
+    let bst = new BST();
+    let rootNode = new BSTNode(36);
+    bst.insert(rootNode);
+    let node2 = new BSTNode(36);
+    expect(bst.insert(node2)).toEqual({"root": {"data": 36, "left": null, "right": null}});
+  }); 
+
+  
 });
